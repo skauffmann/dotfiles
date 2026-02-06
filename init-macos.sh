cef9af72fa7363dc5058e7461daaaf882243efe8
Samuel Kauffmann <skauffmann@tyneo.net>
2026-02-06 08:34:32 +0100

    remove xcode lisence

--- init-macos.sh
+++ init-macos.sh
@@ -22,7 +22,6 @@ echo "Installing Xcode Command Line Tools..."
 /usr/bin/xcode-select --install 2>/dev/null || echo "Already installed."
 echo "Press Enter once the installation is complete."
 read -r
-sudo xcodebuild -license accept
 
 # Homebrew
 if ! command -v brew &>/dev/null; then
